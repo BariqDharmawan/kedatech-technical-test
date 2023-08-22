96ec7378f557f3092e8a08836f147fcf8410d047
fathrahh <gazoon3@gmail.com>
2023-08-22 16:11:15 +0700

    a

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -1,67 +1,64 @@
-'use client';
+"use client";
 
-import Link from 'next/link';
-import '@/styles/globals.scss';
-import type { Metadata } from 'next';
-import { SWRConfig } from 'swr';
-import axios from 'axios';
+import Link from "next/link";
+import "@/styles/globals.scss";
+import type { Metadata } from "next";
+import { SWRConfig } from "swr";
+import axios from "axios";
 
 export const metadata: Metadata = {
-	title: 'Dashboard',
-	description: 'Dashboard app',
+  title: "Dashboard",
+  description: "Dashboard app",
 };
 
 const menus = [
-	{
-		label: 'Home',
-		href: '/dashboard',
-	},
-	{
-		label: 'Customer Basic',
-		href: '/basic',
-	},
-	{
-		label: 'Customer Business',
-		href: '/business',
-	},
-	{
-		label: 'Customer Entrepeneur',
-		href: '/entrepeneur',
-	},
+  {
+    label: "Home",
+    href: "/dashboard",
+  },
+  {
+    label: "Customer Basic",
+    href: "/basic",
+  },
+  {
+    label: "Customer Business",
+    href: "/business",
+  },
+  {
+    label: "Customer Entrepeneur",
+    href: "/entrepeneur",
+  },
 ];
 
 export default function DashboardLayout({
-	children,
+  children,
 }: {
-	children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-	return (
-		<html lang='en'>
-			<body className='grid grid-cols-12'>
-				<aside className='col-span-3 bg-blue-500 h-screen text-white'>
-					{menus.map(menu => (
-						<Link
-							key={`menu-admin-${menu.label.replace(' ', '-')}`}
-							href={menu.href}
-							className='p-3 block capitalize'
-						>
-							{menu.label}
-						</Link>
-					))}
-				</aside>
-				<div className='col-span-9'>
-					<SWRConfig
-						value={{
-							revalidateOnFocus: false,
-							refreshInterval: 0,
-							fetcher: resource =>
-								axios.get(resource).then(res => res.data),
-						}}
-					>
-						{children}
-					</SWRConfig>
-				</div>
-			</body>
-		</html>
-	);
+  return (
+    <div className="grid grid-cols-12">
+      <aside className="col-span-3 bg-blue-500 h-screen text-white">
+        {menus.map((menu) => (
+          <Link
+            key={`menu-admin-${menu.label.replace(" ", "-")}`}
+            href={menu.href}
+            className="p-3 block capitalize"
+          >
+            {menu.label}
+          </Link>
+        ))}
+      </aside>
+      <div className="col-span-9">
+        <SWRConfig
+          value={{
+            revalidateOnFocus: false,
+            refreshInterval: 0,
+            fetcher: (resource) => axios.get(resource).then((res) => res.data),
+          }}
+        >
+          {children}
+        </SWRConfig>
+      </div>
+    </div>
+  );
 }
